2f9ff3a8cbb15964c5c9cbe0206cfe534bfd8e6e
upto3 <>
2024-03-01 17:06:53 +0800

    pass card nft

---
+++ src/PassCardNFT.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract PassCardNFT is ERC721URIStorage {
+    using Strings for uint256;
+
+    string public constant TOKEN_URI = "ipfs://<your-ipfs-hash-here>";
+    uint256 public tokenCounter;
+
+    constructor() ERC721("PassCardNFT", "PCNFT") {
+        tokenCounter = 0;
+    }
+
+    function mint() public {
+		tokenCounter++;
+
+        _mint(msg.sender, tokenCounter);
+        _setTokenURI(tokenCounter, TOKEN_URI);
+    }
+
+    function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
+		address from = _ownerOf(tokenId);
+
+		// Prevent transfers by checking if the token already has an owner (not minting) and not burning
+		if (from != address(0) && to != address(0)) {
+			revert("Transfers are disabled for this NFT");
+		}
+
+		return super._update(to, tokenId, auth);
+	}
+}
