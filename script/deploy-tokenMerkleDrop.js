0100fa596dd2d721a061a2c4fe945f1bd39c8373
bob <>
2024-03-18 17:45:43 +0800

    modify drop devlopment script

--- script/deploy-tokenMerkleDrop.js
+++ script/deploy-tokenMerkleDrop.js
@@ -1,14 +1,16 @@
 const hre = require('hardhat')
 
 async function main() {
-  const drop = await hre.ethers.deployContract('UPTMerkleDrop', [
+  const Drop = await hre.ethers.getContractFactory('UPTMerkleDrop')
+
+  const drop = await hre.upgrades.deployProxy(Drop, [
     '0x08e0948E952063a6396a24fc59554aC476bEa66e',
     '0x333f50702DFB7FC32CFccEA9F587326D27c6E214',
+    '0x4300000000000000000000000000000000000002',
   ])
-
   await drop.waitForDeployment()
 
-  console.log(`Deployed to ${drop.target}`)
+  console.log('drop deployed to:', await drop.getAddress())
 }
 
 main().catch((error) => {
